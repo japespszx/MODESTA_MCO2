976022bf51dee594ba6718895d44a9135f78a665
japespszx <japespszx@live.com.ph>
2017-08-05 00:03:51 +0800

    - Put the Rserve connection in a Singleton Class - Made a Binomial function class

--- src/ph/edu/dlsu/modesta/Main.java
+++ src/ph/edu/dlsu/modesta/Main.java
@@ -7,48 +7,40 @@ import org.rosuda.REngine.Rserve.RserveException;
 public class Main {
 
 	public static void main(String[] args) {
+		//initialize Rserve connection
+		RConnection connection = Rserve.getConnection();
 
-		// Create an R vector in the form of a string.
-		RConnection connection = null;
+//		CardList deck = new CardList();
+//		CardList hand;
+//
+//		// START TRIAL
+//		for (int i = 0; i < 10000; i++) {
+//
+//			System.out.println("===== TRIAL " + (i + 1) + " =====");
+//
+//			hand = new CardList();
+//			deck.resetCards();
+//
+//			hand.addCard(deck.drawCard());
+//			hand.addCard(deck.drawCard());
+//			hand.addCard(deck.drawCard());
+//			hand.addCard(deck.drawCard());
+//			hand.addCard(deck.drawCard());
+//
+//			hand.print();
+//		}
 
 		try {
-             /* Create a connection to Rserve instance running
-              * on default port 6311
-              */
-			connection = new RConnection();
-			//TODO Put code here
-
-			CardList deck = new CardList();
-			CardList hand;
-
-			// START TRIAL
-			for (int i = 0; i < 10000; i++) {
-
-				System.out.println("===== TRIAL " + (i + 1) + " =====");
-
-				hand = new CardList();
-				deck.resetCards();
-
-				hand.addCard(deck.drawCard());
-				hand.addCard(deck.drawCard());
-				hand.addCard(deck.drawCard());
-				hand.addCard(deck.drawCard());
-				hand.addCard(deck.drawCard());
-
-				hand.print();
-			}
-
 			//Sample code
 			String vector = "c(1,2,3,4)";
 			connection.eval("meanVal=mean(" + vector + ")");
 			double mean = connection.eval("meanVal").asDouble();
-			System.out.println("The mean of given vector is=" + mean);
+			System.out.println(Binomial.dbinom(4, 5, .5));
 		} catch (RserveException | REXPMismatchException e) {
 			e.printStackTrace();
-		} finally{
-			if (connection != null) {
+		} finally {
+			if (connection != null)
 				connection.close();
-			}
 		}
 	}
 }
