db346702cd6cbb488fcdb6309196c631aeb73c5d
japespszx <japespszx@live.com.ph>
2017-08-04 22:53:57 +0800

    - Added sample Rserve connection code. - Removed DiscreteProbabilityDistribution

--- src/ph/edu/dlsu/modesta/Main.java
+++ src/ph/edu/dlsu/modesta/Main.java
@@ -1,29 +1,54 @@
 package ph.edu.dlsu.modesta;
 
-import static ph.edu.dlsu.modesta.DiscreteProbabilityDistribution.*;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		CardList deck = new CardList();
-		CardList hand;
+		// Create an R vector in the form of a string.
+		RConnection connection = null;
 
-		// START TRIAL
-		for (int i = 0; i < 10000; i++) {
+		try {
+             /* Create a connection to Rserve instance running
+              * on default port 6311
+              */
+			connection = new RConnection();
+			//TODO Put code here
 
-			System.out.println("===== TRIAL " + (i + 1) + " =====");
+			CardList deck = new CardList();
+			CardList hand;
 
-			hand = new CardList();
-			deck.resetCards();
+			// START TRIAL
+			for (int i = 0; i < 10000; i++) {
 
-			hand.addCard(deck.drawCard());
-			hand.addCard(deck.drawCard());
-			hand.addCard(deck.drawCard());
-			hand.addCard(deck.drawCard());
-			hand.addCard(deck.drawCard());
+				System.out.println("===== TRIAL " + (i + 1) + " =====");
 
-			hand.print();
+				hand = new CardList();
+				deck.resetCards();
+
+				hand.addCard(deck.drawCard());
+				hand.addCard(deck.drawCard());
+				hand.addCard(deck.drawCard());
+				hand.addCard(deck.drawCard());
+				hand.addCard(deck.drawCard());
+
+				hand.print();
+			}
+
+			//Sample code
+			String vector = "c(1,2,3,4)";
+			connection.eval("meanVal=mean(" + vector + ")");
+			double mean = connection.eval("meanVal").asDouble();
+			System.out.println("The mean of given vector is=" + mean);
+		} catch (RserveException | REXPMismatchException e) {
+			e.printStackTrace();
+		} finally{
+			if (connection != null) {
+				connection.close();
+			}
 		}
 	}
 }
