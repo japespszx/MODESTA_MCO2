e1568af4cea66a8b84a8e880a7707e85f0170b82
japespszx <japespszx@live.com.ph>
2017-08-07 00:42:19 +0800

    - MFP now outs prob

--- src/ph/edu/dlsu/modesta/MakeFrequencyMap.java
+++ src/ph/edu/dlsu/modesta/MakeFrequencyMap.java
@@ -2,12 +2,11 @@ package ph.edu.dlsu.modesta;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 class MakeFrequencyMap {
 	public static void main(String[] args) throws IOException {
-		FileWriter w = new FileWriter("FrequencyMap.csv");
-		writeToCSV("drawn", "total", "possible", w);
 		int[] draw1 = new int[14];
 		int[] draw2 = new int[27];
 		int[] draw3 = new int[40];
@@ -85,34 +84,50 @@ class MakeFrequencyMap {
 		}
 
 		System.out.println("1 CARDS DRAWN");
-		for (int i = 1; i <= 13; i++)
-			writeToCSV(1, i, draw1[i], w);
+		FileWriter w = new FileWriter("fwor1.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 13; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw1[i]/52.0), w);
+		}
+		w.close();
 
 		System.out.println("2 CARDS DRAWN");
-		for (int i = 1; i <= 26; i++)
-			writeToCSV(2, i, draw2[i], w);
+		w = new FileWriter("fwor2.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 26; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw2[i]/2652.0), w);
+		}
+		w.close();
 
 		System.out.println("3 CARDS DRAWN");
-		for (int i = 1; i <= 39; i++)
-			writeToCSV(3, i, draw3[i], w);
+		w = new FileWriter("fwor3.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 39; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw3[i]/66300.0), w);
+		}
+		w.close();
 
 		System.out.println("4 CARDS DRAWN");
+		w = new FileWriter("fwor4.csv");
+		writeToCSV("Total", "Possible", w);
 		for (int i = 1; i <= 52; i++) {
-			writeToCSV(4, i, draw4[i], w);
+			writeToCSV(i, BigDecimal.valueOf(draw4[i]/1082900.0), w);
 		}
+		w.close();
 
 		System.out.println("5 CARDS DRAWN");
-		for (int i = 1; i <= 64; i++)
-			writeToCSV(5, i, draw5[i], w);
+		w = new FileWriter("fwor5.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 64; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw5[i]/12994800.0), w);
+		}
+		w.close();
 
 		draw1 = new int[14];
 		draw2 = new int[27];
 		draw3 = new int[40];
 		draw4 = new int[53];
 		draw5 = new int[66];
-
-		w = new FileWriter("FrequencyMap2.csv");
-		writeToCSV("drawn", "total", "possible", w);
 
 		for (int i = 1; i <= 5; i++) {
 			boolean toggle = false;
@@ -183,26 +198,43 @@ class MakeFrequencyMap {
 		}
 
 		System.out.println("1 CARDS DRAWN");
-		for (int i = 1; i <= 13; i++)
-			writeToCSV(1, i, draw1[i], w);
+		w = new FileWriter("fwr1.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 13; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw1[i]/52.0), w);
+		}
+		w.close();
 
 		System.out.println("2 CARDS DRAWN");
-		for (int i = 1; i <= 26; i++)
-			writeToCSV(2, i, draw2[i], w);
+		w = new FileWriter("fwr2.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 26; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw2[i]/2652.0), w);
+		}
+		w.close();
 
 		System.out.println("3 CARDS DRAWN");
-		for (int i = 1; i <= 39; i++)
-			writeToCSV(3, i, draw3[i], w);
+		w = new FileWriter("fwr3.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 39; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw3[i]/66300.0), w);
+		}
+		w.close();
 
 		System.out.println("4 CARDS DRAWN");
+		w = new FileWriter("fwr4.csv");
+		writeToCSV("Total", "Possible", w);
 		for (int i = 1; i <= 52; i++) {
-			writeToCSV(4, i, draw4[i], w);
+			writeToCSV(i, BigDecimal.valueOf(draw4[i]/1082900.0), w);
 		}
+		w.close();
 
 		System.out.println("5 CARDS DRAWN");
-		for (int i = 1; i <= 65; i++)
-			writeToCSV(5, i, draw5[i], w);
-
+		w = new FileWriter("fwr5.csv");
+		writeToCSV("Total", "Possible", w);
+		for (int i = 1; i <= 65; i++) {
+			writeToCSV(i, BigDecimal.valueOf(draw5[i]/12994800.0), w);
+		}
 		w.close();
 	}
 
@@ -223,6 +255,28 @@ class MakeFrequencyMap {
 		values.add(Integer.toString(drawn));
 		values.add(Integer.toString(i));
 		values.add(Integer.toString(possible));
+		try {
+			CSVUtils.writeLine(w, values);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void writeToCSV(String s1, String s2, FileWriter w) {
+		ArrayList<String> values = new ArrayList<>();
+		values.add(s1);
+		values.add(s2);
+		try {
+			CSVUtils.writeLine(w, values);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void writeToCSV(int i, BigDecimal possible, FileWriter w) {
+		ArrayList<String> values = new ArrayList<>();
+		values.add(Integer.toString(i));
+		values.add(possible.toString());
 		try {
 			CSVUtils.writeLine(w, values);
 		} catch (IOException e) {
