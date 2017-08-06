2196c50d3ba9ea89707e7f0578c8da6b50c7576f
Patrick Tobias <patrick_tobias@outlook.com>
2017-08-06 21:45:09 +0800

    Implement card images

--- src/ph/edu/dlsu/modesta/CardDrawView.java
+++ src/ph/edu/dlsu/modesta/CardDrawView.java
@@ -1,11 +1,13 @@
 package ph.edu.dlsu.modesta;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by patricktobias on 05/08/2017.
@@ -22,7 +24,11 @@ public class CardDrawView {
 	private JLabel card4;
 	private JLabel card5;
 
+	private ArrayList<JLabel> cardsDisplay;
+
 	private CardList deck;
+
+	private static final String prefix_images = "/ph/edu/dlsu/modesta/assets/cards/";
 
 	public CardDrawView(CardList deck) {
 		JFrame frame = new JFrame("CardDrawView");
@@ -35,11 +41,31 @@ public class CardDrawView {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 
+		frame.setSize(800, 400);
+
 		handSize.addItem("1");
 		handSize.addItem("2");
 		handSize.addItem("3");
 		handSize.addItem("4");
 		handSize.addItem("5");
+
+		cardsDisplay = new ArrayList<>();
+
+		ImageIcon image = new ImageIcon(getClass().getResource(prefix_images + "others/Back.PNG"));
+
+		cardsDisplay.add(card5);
+		cardsDisplay.add(card4);
+		cardsDisplay.add(card3);
+		cardsDisplay.add(card2);
+		cardsDisplay.add(card1);
+
+		card1.setIcon(optimizeSize(image));
+		card2.setIcon(optimizeSize(image));
+		card3.setIcon(optimizeSize(image));
+		card4.setIcon(optimizeSize(image));
+		card5.setIcon(optimizeSize(image));
+
+		frame.repaint();
 
 		addListenerToDrawBtn(e -> {
 
@@ -73,6 +99,8 @@ public class CardDrawView {
 			int totalTally_with = 0;
 			int totalTally_without = 0;
 
+			String card_filename;
+
 			for (int i = 0; i < Integer.parseInt(iterations.getText()); i++) {
 
 				csvLine = new ArrayList<>();
@@ -86,6 +114,13 @@ public class CardDrawView {
 
 				for (int j = 0; j < (Integer.parseInt((String) handSize.getSelectedItem())); j++) {
 					Card card = hand.addCard(deck.drawCard());
+
+					card_filename = card.getSuit().getSuit() + "-" + card.getNumber() + ".PNG";
+
+					cardsDisplay.get(j).setIcon(optimizeSize(
+							new ImageIcon(getClass().getResource(
+									prefix_images + card.getSuit().getSuit() + "/" + card_filename
+							))));
 
 					csvLine.add(card.getSuit().getSuit() + ":" + card.getNumber());
 				}
@@ -116,6 +151,13 @@ public class CardDrawView {
 
 				for (int j = 0; j < (Integer.parseInt((String) handSize.getSelectedItem())); j++) {
 					Card card = hand.addCard(deck.drawCardAndReturn());
+
+					card_filename = card.getSuit().getSuit() + "-" + card.getNumber() + ".PNG";
+
+					cardsDisplay.get(j).setIcon(optimizeSize(
+							new ImageIcon(getClass().getResource(
+									prefix_images + card.getSuit().getSuit() + "/" + card_filename
+							))));
 
 					csvLine.add(card.getSuit().getSuit() + ":" + card.getNumber());
 				}
@@ -158,5 +200,12 @@ public class CardDrawView {
 
 	public void addListenerToDrawBtn(ActionListener actionListener) {
 		DRAWButton.addActionListener(actionListener);
+	}
+
+	private ImageIcon optimizeSize (ImageIcon imageIcon) {
+		Image imageObject = imageIcon.getImage(); // transform it
+		Image newimg = imageObject.getScaledInstance(126, 183,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+
+		return new ImageIcon(newimg);
 	}
 }
