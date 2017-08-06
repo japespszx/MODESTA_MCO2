e590bc18e4abda0f05e778a446f54f64ec91527d
Patrick Tobias <patrick_tobias@outlook.com>
2017-08-06 09:48:13 +0800

    Produce logs for with and without replacement

--- src/ph/edu/dlsu/modesta/CardDrawView.java
+++ src/ph/edu/dlsu/modesta/CardDrawView.java
@@ -3,6 +3,10 @@ package ph.edu.dlsu.modesta;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by patricktobias on 05/08/2017.
@@ -42,10 +46,40 @@ public class CardDrawView {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String wReplace_Log = "wReplace_log.csv";
+                String woReplace_Log = "withoutReplace_log.csv";
+                FileWriter writer_w = null;
+                FileWriter writer_wo = null;
+
+                ArrayList<String> csvLine = new ArrayList<String>();
+
+                csvLine.add("Trial Number");
+
+                for (int j = 0; j < (Integer.parseInt((String) handSize.getSelectedItem())); j++ ) {
+                    csvLine.add("Card" + (j+1));
+                }
+
+                csvLine.add("Total");
+                csvLine.add("Total = Desired Total");
+
+                try {
+                    writer_w = new FileWriter(wReplace_Log);
+                    writer_wo = new FileWriter(woReplace_Log);
+
+                    CSVUtils.writeLine(writer_w, csvLine);
+                    CSVUtils.writeLine(writer_wo, csvLine);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
                 int totalTally_with = 0;
                 int totalTally_without = 0;
 
                 for (int i = 0; i < Integer.parseInt(iterations.getText()); i++) {
+
+                    csvLine = new ArrayList<String>();
+
+                    csvLine.add(i + "");
 
                     System.out.println("===== TRIAL " + (i + 1) + " =====");
 
@@ -53,30 +87,72 @@ public class CardDrawView {
                     deck.resetCards();
 
                     for (int j = 0; j < (Integer.parseInt((String) handSize.getSelectedItem())); j++ ) {
-                        hand.addCard(deck.drawCard());
+                        Card card = hand.addCard(deck.drawCard());
+
+                        csvLine.add(card.getSuit().getSuit() + ":" + card.getNumber());
                     }
 
                     System.out.println("===== Without replacement =====");
                     hand.print();
 
-                    if (Integer.parseInt(targetTotal.getText()) == hand.getTotal())
+                    csvLine.add(hand.getTotal() + "");
+
+                    if (Integer.parseInt(targetTotal.getText()) == hand.getTotal()) {
                         totalTally_without++;
+                        csvLine.add("YES");
+                    } else {
+                        csvLine.add("NO");
+                    }
+
+                    try {
+                        CSVUtils.writeLine(writer_wo, csvLine);
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+
+                    csvLine = new ArrayList<String>();
+
+                    csvLine.add(i + "");
 
                     hand = new CardList();
 
                     for (int j = 0; j < (Integer.parseInt((String) handSize.getSelectedItem())); j++ ) {
-                        hand.addCard(deck.drawCardAndReturn());
-                    }
+                        Card card = hand.addCard(deck.drawCardAndReturn());
 
-                    if (Integer.parseInt(targetTotal.getText()) == hand.getTotal())
-                        totalTally_with++;
+                        csvLine.add(card.getSuit().getSuit() + ":" + card.getNumber());
+                    }
 
                     System.out.println("===== With replacement =====");
                     hand.print();
+
+                    csvLine.add(hand.getTotal() + "");
+
+                    if (Integer.parseInt(targetTotal.getText()) == hand.getTotal()) {
+                        totalTally_with++;
+                        csvLine.add("YES");
+                    } else {
+                        csvLine.add("NO");
+                    }
+
+                    try {
+                        CSVUtils.writeLine(writer_w, csvLine);
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
                 }
 
                 System.out.println("Target total obtained with replacement: " + totalTally_with);
                 System.out.println("Target total obtained without replacement: " + totalTally_without);
+
+                try {
+                    writer_w.flush();
+                    writer_wo.flush();
+
+                    writer_w.close();
+                    writer_wo.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         });
     }
