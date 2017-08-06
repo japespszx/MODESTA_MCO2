1a6c62c769d29fe2c73471c338d8afabe491c872
japespszx <japespszx@live.com.ph>
2017-08-06 17:05:36 +0800

    - FrequencyMap generator is done

--- src/ph/edu/dlsu/modesta/MakeFrequencyMap.java
+++ src/ph/edu/dlsu/modesta/MakeFrequencyMap.java
@@ -18,7 +18,7 @@ class MakeFrequencyMap {
 			values.add("1");
 			values.add(Integer.toString(i));
 			values.add("1");
-
+			CSVUtils.writeLine(w, values);
 		}
 
 		int drawn = 2;
@@ -32,7 +32,7 @@ class MakeFrequencyMap {
 			}
 		}
 
-		for (int i = 1; i <= 26; i++) {
+		for (int i = 1; i < 27; i++) {
 			values = new ArrayList<>();
 			values.add(Integer.toString(drawn));
 			values.add(Integer.toString(i));
@@ -53,7 +53,7 @@ class MakeFrequencyMap {
 			}
 		}
 
-		for (int i = 1; i <= 39; i++) {
+		for (int i = 1; i < 40; i++) {
 			values = new ArrayList<>();
 			values.add(Integer.toString(drawn));
 			values.add(Integer.toString(i));
@@ -62,19 +62,49 @@ class MakeFrequencyMap {
 		}
 
 		drawn = 4;
-		possible = new int[40]; //possible[0] won't be used
+		possible = new int[53]; //possible[0] won't be used
 		for (int i = 1; i <= 13; i++) {
 			for (int j = i; j <= 13; j++) {
 				for (int k = j; k <= 13; k++) {
-					for (int l = 1; l < 40; l++) {
-						if (i + j + k == l)
-							possible[l]++;
+					for (int l = k; l <= 13; l++) {
+						for (int m = 1; m < 53; m++) {
+							if (i + j + k + l == m)
+								possible[m]++;
+						}
 					}
 				}
 			}
 		}
 
-		for (int i = 1; i <= 39; i++) {
+		for (int i = 1; i < 53; i++) {
+			values = new ArrayList<>();
+			values.add(Integer.toString(drawn));
+			values.add(Integer.toString(i));
+			values.add(Integer.toString(possible[i]));
+			CSVUtils.writeLine(w, values);
+		}
+
+		drawn = 5;
+		possible = new int[65]; //possible[0] won't be used
+		for (int i = 1; i <= 13; i++) {
+			for (int j = i; j <= 13; j++) {
+				for (int k = j; k <= 13; k++) {
+					for (int l = k; l <= 13; l++) {
+						for (int m = l; m <= 13; m++) {
+							for (int n = 1; n < 65; n++) {
+								if (isAllEqual(i, j, k, l, m))
+									continue;
+
+								if (i + j + k + l + m == n)
+									possible[n]++;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i < 65; i++) {
 			values = new ArrayList<>();
 			values.add(Integer.toString(drawn));
 			values.add(Integer.toString(i));
@@ -83,5 +113,9 @@ class MakeFrequencyMap {
 		}
 
 		w.close();
+	}
+
+	private static boolean isAllEqual(int i, int j, int k, int l, int m) {
+		return i == j && j == k && k == l && l == m;
 	}
 }
